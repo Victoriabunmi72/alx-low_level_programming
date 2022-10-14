a4769f80c55b4a844dd54f192f5211ea0215392b
Victoriabunmi72 <bhummhie97@gmail.com>
2022-10-14 08:26:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,13 +9,8 @@
  *    *Return: By default returns zero if no erros otherwise returns non-zero value.
  *
  *     */
-
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
