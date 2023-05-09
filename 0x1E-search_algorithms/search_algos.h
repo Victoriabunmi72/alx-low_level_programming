8dad2d99246569e864e3827c6f6347713b2ad9d7
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-09 18:31:28 +0100

    search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * prototypes
+ **/
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
