084c8ae519c4369010551022e33bd1375b7dfcba
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-10 20:25:49 +0100

    1-binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-
 /**
 * binary_search - searches for a value in a sorted array of inteegers
 * @array: pointers to the first element of the array to search
@@ -8,35 +7,34 @@
 *
 * Return: index where value is located, or -1 if it is not found
 */
+
 int binary_search(int *array, size_t size, int value)
 {
-        size_t loweridx = 0;
-        size_t upperidx = size -1;
-        size_t mid;
-        size_t i;
+	size_t loweridx = 0;
+	size_t upperidx = size - 1;
+	size_t mid;
+	size_t i;
+	if (array == NULL)
+		return (-1);
+	while (loweridx <= upperidx)
+	{
+		printf("Searching in array: ");
+		for (i = loweridx; i <= upperidx; i++)
+		{
+			printf("%d", array[i]);
+			if (i != upperidx)
+				printf(", ");
+		}
+		printf("\n");
 
-        if (array == NULL)
-                return (-1);
+		mid = (loweridx + upperidx) / 2;
 
-        while (loweridx <= upperidx)
-        {
-                printf("Searching in array: ");
-                for (i = loweridx; i <= upperidx; i++)
-                {
-                        printf("%d", array[i]);
-                        if (i != upperidx)
-                                printf(", ");
-                }
-                printf("\n");
-
-                mid = (loweridx + upperidx) / 2;
-
-                if (array[mid] == value)
-                        return (mid);
-                else if (array[mid] < value)
-                        loweridx = mid + 1;
-                else
-                        upperidx = mid - 1;
-        }
-        return (-1);
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			loweridx = mid + 1;
+		else
+			upperidx = mid - 1;
+	}
+	return (-1);
 }
