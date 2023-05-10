fa7755ce94eb9403ec3dce8eec4e6eded56933ed
Victoriabunmi72 <bhummhie97@gmail.com>
2023-05-10 18:05:38 +0100

    search_algos.h

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,11 +7,13 @@
  * @value: value to search.
  * Return: Always EXIT_SUCCESS.
  */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
 	if (array == NULL)
+
 	{
 		return (-1);
 	}
